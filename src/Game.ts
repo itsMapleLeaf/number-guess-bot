030aa97c19fec6f71ef1ea4281a6f52b6cfe5189
kingdaro <kingdaro@gmail.com>
2020-01-29 00:36:31 -0600

    use state classes

--- src/Game.ts
+++ src/Game.ts
@@ -1,64 +1,63 @@
 import { randomRange } from "./randomRange"
 
-type GameState = { type: "idle" } | { type: "running"; number: number }
-
 export type GameCommand = { type: "start" } | { type: "guess"; guess: number }
 
 export type GameResult = "newGame" | "tooHigh" | "tooLow" | "finish"
 
+type GameState = {
+  handleCommand(command: GameCommand): GameResult | undefined
+}
+
 export class Game {
-  private state: GameState = { type: "idle" }
+  private state: GameState = new IdleState(this)
 
-  start() {
-    this.state = { type: "running", number: randomRange(0, 100) }
-  }
-
-  finish() {
-    this.state = { type: "idle" }
+  setState(state: GameState) {
+    this.state = state
   }
 
   handleCommand(command: GameCommand): GameResult | undefined {
-    switch (this.state.type) {
-      case "idle":
-        switch (command.type) {
-          case "start":
-            this.start()
-            return "newGame"
+    return this.state.handleCommand(command)
+  }
+}
 
-          default:
-            return
-        }
+class IdleState implements GameState {
+  constructor(private game: Game) {}
 
-      case "running":
-        switch (command.type) {
-          case "guess":
-            return this.handleGuessCommand(this.state.number, command.guess)
+  handleCommand(command: GameCommand): GameResult | undefined {
+    if (command.type === "start") {
+      this.game.setState(new RunningState(this.game))
+      return "newGame"
+    }
+  }
+}
 
-          default:
-            return
-        }
+class RunningState implements GameState {
+  private number = randomRange(0, 100)
+
+  constructor(private game: Game) {}
+
+  handleCommand(command: GameCommand): GameResult | undefined {
+    if (command.type === "guess") {
+      return this.handleGuess(command.guess)
     }
   }
 
-  isValidGuess(guess: number) {
+  private handleGuess(guess: number): GameResult | undefined {
+    if (!this.isValidGuess(guess)) return
+
+    if (guess > this.number) return "tooHigh"
+    if (guess < this.number) return "tooLow"
+
+    this.game.setState(new IdleState(this.game))
+    return "finish"
+  }
+
+  private isValidGuess(guess: number) {
     return (
       Number.isFinite(guess) &&
       Number.isInteger(guess) &&
       guess >= 0 &&
       guess <= 100
     )
-  }
-
-  private handleGuessCommand(
-    number: number,
-    guess: number,
-  ): GameResult | undefined {
-    if (!this.isValidGuess(guess)) return
-
-    if (guess > number) return "tooHigh"
-    if (guess < number) return "tooLow"
-
-    this.finish()
-    return "finish"
   }
 }
